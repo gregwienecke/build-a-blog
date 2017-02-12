5cac5feb3ca77bb7bed420508b080562d84d87c9
Greg Wienecke <gregwienecke@gmail.com>
2017-02-11 23:14:37 -0600

    changed StringProperty to TextProptery in the Model Entry class

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Handler(webapp2.RequestHandler):
 
 class Entry(db.Model):
 	title = db.StringProperty(required = True)
-	body = db.StringProperty(required = True)
+	body = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
 
 
